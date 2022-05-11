160e8cb35413dacb405c73aee3ff4818325c33b9
lucaselcura <94176577+lucaselcura@users.noreply.github.com>
2022-05-11 13:14:47 -0300

    Update UsuarioService.java

--- src/main/java/com/proyecto/proyecto/services/UsuarioService.java
+++ src/main/java/com/proyecto/proyecto/services/UsuarioService.java
@@ -1,4 +1,36 @@
 package com.proyecto.proyecto.services;
 
+import com.proyecto.proyecto.models.Educacion;
+import com.proyecto.proyecto.models.Usuario;
+import com.proyecto.proyecto.repository.UsuarioRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional
+
 public class UsuarioService {
+    prvate final UsuarioRepo usuarioRepo;
+
+    @Autowired
+    public UsuarioService (UsuarioRepo usuarioRepo){
+        this.usuarioRepo=usuarioRepo;
+    }
+    public Usuario addUser(Usuario usuario){
+
+        return usuarioRepo.save(usuario);
+    }
+    public List<Usuario> buscarUsuario (){
+
+        return usuarioRepo.findAll();
+    }
+    public Usuario editarUsuario (Usuario usuario){
+        return usuarioRepo.save(usuario);
+    }
+    public void borrarUsuario(Long id) {
+        usuarioRepo.deleteById(id);
+    }
 }
