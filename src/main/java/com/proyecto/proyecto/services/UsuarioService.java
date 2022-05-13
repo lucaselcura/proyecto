16f6eb785f621d2ce13cc39fe16eff7b290d06de
lucaselcura <94176577+lucaselcura@users.noreply.github.com>
2022-05-13 15:08:55 -0300

    Base conectada

--- src/main/java/com/proyecto/proyecto/services/UsuarioService.java
+++ src/main/java/com/proyecto/proyecto/services/UsuarioService.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.nio.file.attribute.UserPrincipalNotFoundException;
 import java.util.List;
 
 @Service
